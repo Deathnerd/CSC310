b19974591f691d0ff27fffbc93fcc53520c62aa3
Wes Gilleland <wes.gilleland@gmail.com>
2015-10-21 21:07:47 -0400

    Added display methods that utilize varargs

--- src/com/gilleland/george/utils/Menu.java
+++ src/com/gilleland/george/utils/Menu.java
@@ -15,6 +15,7 @@ public class Menu {
      * Display a Menu for program navigation with a 0 exit option
      *
      * @param choices The choices to display
+     * @return The user's numeric choice
      */
     public static int display(ArrayList<String> choices) {
         int i = 1;
@@ -27,7 +28,24 @@ public class Menu {
     }
 
     /**
+     * Display a menu for program navigation with a 0 exit option
+     *
+     * @param choices Any number of strings to display as choices
+     * @return The user's numeric choice
+     */
+    public static int display(String... choices) {
+        int i = 1;
+        for (String choice : choices) {
+            System.out.printf("%d. %s\n", i++, choice);
+        }
+        System.out.println("0. Exit");
+        System.out.println(Menu.default_prompt);
+        return Menu.in.nextInt();
+    }
+
+    /**
      * Returns the canonical string value of the menu item chosen
+     *
      * @param choices The list of choices to display
      * @return A null string if the exit was chosen, otherwise the canonical string value of the menu item
      */
@@ -39,26 +57,47 @@ public class Menu {
         System.out.println("0. Exit");
         System.out.println(Menu.default_prompt);
         int choice = Menu.in.nextInt() - 1;
-        if(choice != -1){
+        if (choice != -1) {
             return choices.get(choice);
         }
         return "";
     }
 
     /**
+     * Returns the canonical string value of the menu item chosen
+     *
+     * @param choices Any number of strings to display as choices
+     * @return A null string if the exit was chosen, otherwise the canonical string value of the menu item
+     */
+    public static String displayS(String... choices) {
+        int i = 1;
+        for (String choice : choices) {
+            System.out.printf("%d. %s\n", i++, choice);
+        }
+        System.out.println("0. Exit");
+        System.out.println(Menu.default_prompt);
+        int choice = Menu.in.nextInt() - 1;
+        if (choice != -1) {
+            return choices[choice];
+        }
+        return "";
+    }
+
+    /**
      * Displays a menu to perform associated callbacks.
+     *
      * @param callbacks LinkedHashMap of keys and their associated callbacks to perform
      * @return Returns the displayed number beside the choice in the menu
      * @throws CallbackFailedException If the callback failed for any reason
      */
-    public static int display(LinkedHashMap<String, HomeworkAssignment> callbacks) throws CallbackFailedException{
+    public static int display(LinkedHashMap<String, HomeworkAssignment> callbacks) throws CallbackFailedException {
         ArrayList<String> arrayListOfKeys = Utils.getArrayListOfKeys(callbacks);
         String choice = displayS(arrayListOfKeys);
-        try{
-            if(!choice.equals("")){
+        try {
+            if (!choice.equals("")) {
                 (callbacks.get(choice)).run();
             }
-        } catch(Exception e){
+        } catch (Exception e) {
             throw new CallbackFailedException();
         }
         // Returns the displayed number beside the choice in the menu
@@ -68,10 +107,10 @@ public class Menu {
     /**
      * Displays a displayWithCallbacks and attempts to run the callback associated with the choice
      *
-     * @deprecated Use {@link #display(LinkedHashMap)} instead
      * @param choices   The choices to display
      * @param callbacks The callbacks for each choice for the user to run
      * @return The choice that the user entered
+     * @deprecated Use {@link #display(LinkedHashMap)} instead
      */
     @Deprecated
     public static int displayWithCallbacks(ArrayList<String> choices, ArrayList<HomeworkAssignment> callbacks) {
