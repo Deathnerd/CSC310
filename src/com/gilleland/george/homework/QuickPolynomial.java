d887367149aeb905539102519a2df781673d6c12
Wes Gilleland <wes.gilleland@gmail.com>
2015-10-28 18:57:43 -0400

    Cleaned up and commented everything. Added a delete function with O(1) complexity

--- src/com/gilleland/george/homework/QuickPolynomial.java
+++ src/com/gilleland/george/homework/QuickPolynomial.java
@@ -13,8 +13,8 @@ import com.gilleland.george.utils.Utils;
  * grasp on the concept of linked lists and can implement them. I have gone through
  * and cleaned up the code by adding expressive method/property names, and adding some
  * helpful comments.
- * <p>
- * Created by Wes Gilleland on 10/27/2015.
+ *
+ * @author Wes Gilleland
  */
 public class QuickPolynomial {
     private QuickTerm head;
@@ -31,35 +31,39 @@ public class QuickPolynomial {
      * Prints out the string representation of the current polynomial expression
      */
     void print() {
-        Helper helper = new Helper(null, head);
-        QuickTerm current = helper.current;
+        Helper helper = new Helper(null, this.head);
         String polynomial = "";
-        while (current != null) {
-            polynomial += this.ensureSignage(current.getData()) + "x^" + current.getKey();
+        while (helper.current != null) {
+            polynomial += this.ensureSignage(helper.current.getData()) + "x^" + helper.current.getKey();
             helper.moveNext();
-            current = helper.current;
         }
         System.out.printf("The current polynomial is: %s \n", polynomial);
     }
 
     /**
-     * Empty the polynomial
+     * Empty the polynomial linked list by destroying the head of the chain
      */
     void empty() {
         this.head = null;
     }
 
     /**
-     * Search for the first node with the matching key
+     * Search for the first node with the matching key and return it.
      *
      * @param key The key value to search for
      * @return references to the node and its predecessor if key is found, otherwise
      * will return null
+     * @throws TermNotFoundException when the search is exhausted
      */
     Helper search(int key) throws TermNotFoundException {
-        //search starts from  left to right
-        Helper helper = new Helper(null, head);
-        while (helper.current != null && helper.current.getKey() >= key) {
+        Helper helper = new Helper(null, this.head);
+
+        /*
+         * Starting at the beginning, iterate over the linked list's
+         * structure, checking for the key given. If it's found, return
+         * the node as a Helper object. Otherwise, throw a TermNotFoundException
+         */
+        while (helper.current != null) {
             if (key == helper.current.getKey()) {
                 return helper;
             }
@@ -69,21 +73,40 @@ public class QuickPolynomial {
     }
 
     /**
-     * Insert a node (exponent, coefficient) into the list
+     * Insert a node (exponent, coefficient) into the list. This will also take care of
+     * inserting new data into existing keys
      *
      * @param exponent    The exponent for the current term that the node is representing
      * @param coefficient The coefficient for the current term that the node is representing
      */
     void insert(int exponent, String coefficient) {
-        Helper helper = new Helper(null, head);
+        Helper helper = new Helper(null, this.head);
 
+        /*
+         * From the start of the linked list, search for the term that has the key we're looking for
+         * while also making sure that the insertion order of greatest key to least key is preserved
+         */
         while (helper.current != null && exponent < helper.current.getKey()) {
             helper.moveNext();
         }
 
+        // Make the new term to insert
         QuickTerm newTerm = new QuickTerm(exponent, coefficient);
+        /*
+         * Here's where the magic happens. First, check if the node we stopped at is not null to prevent
+         * null pointer exceptions, and that its key is equal to our given key. If so, then update its
+         * data with the new value.
+         *
+         * If the above fails and we're at the head of the non-empty linked list (helper.previous == null),
+         * then set the reference for our new term's next pointer to our current head and set the head of
+         * our linked list to the new term. This will in effect insert the new term (node) into the beginning
+         * of our list.
+         *
+         * If both of those aren't conditions to be met, then we're inserting our new term into the space between
+         * two existing nodes in the list. Set the previous node's next reference to our new node, and set the
+         * new node's next reference pointer to our current term.
+         */
         if (helper.current != null && helper.current.getKey() == exponent) {
-            helper.current.setKey(exponent);
             helper.current.setData(coefficient);
         } else if (helper.previous == null) {       //new node should become the first in the list
             newTerm.setNext(head);
@@ -92,6 +115,7 @@ public class QuickPolynomial {
             helper.previous.setNext(newTerm);
             newTerm.setNext(helper.current);
         }
+        // We've added a new term so we must increase our current size for easy reference
         this.length++;
     }
 
@@ -106,15 +130,19 @@ public class QuickPolynomial {
     void addition(int exponent, String coefficient) {
         Helper helper = new Helper(null, this.head);
         coefficient = this.ensureSignage(coefficient);
+        /*
+         * Starting with the head, check if the exponent given matches the current node's
+         * exponent. If so, then we need to calculate the result of adding the two coefficients
+         * together. If that would equal zero, then there's no point for the new coefficient
+         * to be in the final result and should be deleted. Otherwise if the key is found and the
+         * new coefficient isn't 0, then the current node's data needs to be updated with the new
+         * coefficient value.
+         */
         while (helper.current != null) {
             if (helper.current.getKey() == exponent) {
                 coefficient = String.valueOf(Integer.parseInt(helper.current.getData()) + Integer.parseInt(coefficient));
                 if (Integer.parseInt(coefficient) == 0) {
-                    try {
-                        this.delete(helper.current.getKey());
-                    } catch (TermNotFoundException e) {
-                        //
-                    }
+                    this.delete(helper);
                     return;
                 }
                 helper.current.setData(this.ensureSignage(coefficient));
@@ -122,6 +150,11 @@ public class QuickPolynomial {
             }
             helper.moveNext();
         }
+
+        /*
+         * If the end of the list is reached, then a term with that exponent power does not
+         * exist and should be inserted as a new term.
+         */
         this.insert(exponent, coefficient);
     }
 
@@ -134,18 +167,21 @@ public class QuickPolynomial {
      * @param coefficient The coefficient of the term
      */
     void subtraction(int exponent, String coefficient) {
-        Helper helper = new Helper(null, head);
+        Helper helper = new Helper(null, this.head);
         coefficient = this.ensureSignage(coefficient);
 
+        /*
+         * Starting at the beginning of the list, iterate over each node. If a node with the
+         * given key (exponent) is reached, then calculate the result of subtracting the given
+         * coefficient from the current node's coefficient. If that operation would yield a result
+         * of 0, then the current node should be deleted (de-referenced) from the link chain. Otherwise
+         * if the result was non-zero, set the current node's data to the result's value.
+         */
         while (helper.current != null) {
             if (helper.current.getKey() == exponent) {
                 coefficient = String.valueOf(Integer.parseInt(helper.current.getData()) - Integer.parseInt(coefficient));
                 if (Integer.parseInt(coefficient) == 0) {
-                    try {
-                        this.delete(helper.current.getKey());
-                    } catch (TermNotFoundException e) {
-                        //
-                    }
+                    this.delete(helper);
                     return;
                 }
                 helper.current.setData(this.ensureSignage(coefficient));
@@ -153,38 +189,88 @@ public class QuickPolynomial {
             }
             helper.moveNext();
         }
+        /*
+         * If the end of the list is reached, then a term with the exponent given does not
+         * exist and should be inserted as a new term. However, since this is subtraction, the
+         * coefficient should be inverted before being inserted.
+         */
         this.insert(exponent, this.ensureSignage("" + (Integer.parseInt(coefficient) * (-1))));
     }
 
     /**
      * Deletes the first occurrence of a node whose key (exponent) matches the
-     * one passed.
+     * one passed. Calls {@link #search(int)} to find the term to delete
+     * and does not encapsulate the TermNotFoundException thrown by it. This was
+     * done because you should be aware that you might be attempting to delete,
+     * which is technically an access, a node that's not there. Opening up the black
+     * box for this does not violate OOP standards.
      *
      * @param exponent The exponent (key) to search by
+     * @throws TermNotFoundException if trying to delete a term that doesn't exist with the given key
      */
     void delete(int exponent) throws TermNotFoundException {
-        Helper helper = this.search(exponent);
+        /*
+         * Search for the term in the linked list by exception. Will throw a
+         * TermNotFoundException if it cannot find one.
+         */
+        delete(this.search(exponent));
+    }
 
-        final QuickTerm next = helper.current.getNext();
-        final QuickTerm prev = helper.previous;
+    /**
+     * Given a node, delete it from the linked list and decrement the count
+     *
+     * @param node The node to delete
+     */
+    public void delete(Helper node) {
+        // hold the current node's previous and next references
+        final QuickTerm next = node.current.getNext();
+        final QuickTerm prev = node.previous;
 
+        /*
+         * If the previous node reference is not null, then we can just override
+         * its next pointer to point to the next pointer pointed by current's next
+         * pointer. This will effectively skip over the current node and leave it
+         * dangling in the reference chain in memory for the garbage collector to
+         * clean up.
+         *
+         * Otherwise, if the previous pointer is null then we're at the first element
+         * and and we can just replace our head element with the current element's next element.
+         * This will also leave the current node unreachable and dangling for the
+         * garbage collector to clean up.
+         */
         if (prev != null) {
             prev.setNext(next);
         } else {
             this.head = next;
         }
-
+        // Decrement the member counter on successful deletion
         this.length--;
     }
 
     /**
-     * Ensures that there are signs in front of the coefficients
+     * Ensures that there are signs in front of the coefficients.
+     * <p>
+     * For example, given that you pass a string "4", then "+4" will
+     * be returned. Does not need to be concerned with 0 because it is never
+     * called under a situation where it can have a 0 passed to it or where
+     * returning a +0 will be of a concern because if a coefficient is 0 then
+     * the node is deleted from the linked list (polynomial). Strings with
+     * "-" as the first character will fall through
      *
      * @param coefficient The (possibly) unsigned number as a string
      * @return The signed number as a string
      */
     private String ensureSignage(String coefficient) {
         return (!Utils.isOperator(coefficient.charAt(0))) ? "+" + coefficient : coefficient;
+    }
+
+    /**
+     * A simple check on the length property to determine if the linked list is empty
+     *
+     * @return Whether the length of the linked list is 0 or not
+     */
+    public boolean isEmpty() {
+        return this.length == 0;
     }
 }
 
